77535e9dd62ebb09ac1a8c199cc314bed3245ed3
Mohammad Khalil <moegts@gmail.com>
2021-10-05 14:22:50 +0300

    sorrrrry i am saddd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { withAuth0 } from '@auth0/auth0-react'
 export class App extends Component {
   render() {
+    const { isAuthenticated } = this.props.auth0;
     return (
       <div>
+        {isAuthenticated ? <h }
         {/* @todo show login button and hide the list for unauthenticated users */}
         {/* @todo show logout button and show items list components for authenticated users */}
       </div>
